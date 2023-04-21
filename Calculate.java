9d67714b1775ea2e74e18356ea32e5a4bdcf77af
prabhat-pr <prab_24@ymail.com>
2023-04-21 13:46:38 +0530

    uploaded files

---
+++ Calculate.java
@@ -0,0 +1,9 @@
+public class Calculate {
+
+	public static void main(String[] args) {
+		int a = 10, b = 10;
+		int sum = a+b;
+		System.out.println(sum);
+System.out.println("Values: "+a+" "+b);
+	}
+}
---
+++ IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+
+	public static void main(String[] args) {
+		int age = 18;
+		
+		if(age >= 18) {
+			System.out.println("Elgible to drive");
+		} else {
+			System.out.println("Not elgible to drive");
+		}
+
+	}
+
+}
